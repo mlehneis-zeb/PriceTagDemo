d594445cd45063ae863c27c717fb2ee9e82788bc
Lehneis <mlehneis@zeb.de>
2018-11-11 14:59:47 +0100

    Inlcude Google Translate

--- PriceTag-Functions/functions/index.js
+++ PriceTag-Functions/functions/index.js
@@ -1,4 +1,15 @@
 const functions = require('firebase-functions');
+// Imports the Google Cloud client library
+const {Translate} = require('@google-cloud/translate');
+
+// Your Google Cloud Platform project ID
+const projectId = 'pricetag-bce06cd';
+
+// Instantiates a client
+const translate = new Translate({
+  projectId: projectId,
+});
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
